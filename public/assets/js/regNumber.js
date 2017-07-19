b9d2eacb89669e292892bbfeb5d318c2a974e69c
Itzel <itzeljimenez.do@gmail.com>
2017-07-19 14:29:57 +0000

    validation

--- public/assets/js/regNumber.js
+++ public/assets/js/regNumber.js
@@ -1,24 +1,30 @@
-
+const url = 'http://localhost:3000/api/registerNumber';
 const $form = $('#form');
 const $send = $('#send');
 const $phone = $('#phone');
+const $checkbox = $('#test1');
 
 const loadPage = () =>{
 	$form.submit(validate);
 	$phone.keydown(validate);
+	$send.click(apiRequest);
+	$checkbox.click(validate);
 }
 
 
 const validate = ()=>{
-
 	let $phoneVal = $phone.val();
-	let $checkbox = $('#test1').is(':checked');
+	let $checked = $checkbox.is(':checked');
 	console.log($phoneVal.length);
-	if($phoneVal.length == 8 && $checkbox){
+	if( $phoneVal.length == 10 && $checked  ){
 		console.log($checkbox);
 		$send.removeClass('disabled');
 	} else {
 		$send.addClass('disabled');
 	}
+}
+
+const apiRequest = () =>{
+
 }
 $(document).ready(loadPage);
