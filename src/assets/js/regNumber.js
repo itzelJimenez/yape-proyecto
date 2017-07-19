efde38c0961546e03ebdfe2d4ed2c76a6edd2d7c
Itzel <itzeljimenez.do@gmail.com>
2017-07-19 13:07:39 +0000

    validacion con detalles

--- src/assets/js/regNumber.js
+++ src/assets/js/regNumber.js
@@ -1,1 +1,24 @@
-alert("regNumer")
+
+const $form = $('#form');
+const $send = $('#send');
+const $phone = $('#phone');
+
+const loadPage = () =>{
+	$form.submit(validate);
+	$phone.keydown(validate);
+}
+
+
+const validate = ()=>{
+
+	let $phoneVal = $phone.val();
+	let $checkbox = $('#test1').is(':checked');
+	console.log($phoneVal.length);
+	if($phoneVal.length == 8 && $checkbox){
+		console.log($checkbox);
+		$send.removeClass('disabled');
+	} else {
+		$send.addClass('disabled');
+	}
+}
+$(document).ready(loadPage);
